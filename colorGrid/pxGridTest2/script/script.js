e1651396a96240755d644b59fcfc07b1ee0c45e2
Karl-Gustav <47669176+Gizyz@users.noreply.github.com>
2023-07-26 23:52:05 +0200

    Add files via upload

---
+++ colorGrid/pxGridTest2/script/script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
+
+function setCanvas(x, y) {
+    canvas.width = x
+    canvas.height = y
+}
+
+
+setCanvas(20000, 20000);
