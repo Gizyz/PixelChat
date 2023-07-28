326cced3782740b1d5f485b6b599179fdd4e862d
KarlGustav <thommessenka@gmail.com>
2023-07-28 14:16:37 +0200

    animation soo cooool

--- colorGrid/pxGridTest2/script/script.js
+++ colorGrid/pxGridTest2/script/script.js
@@ -15,30 +15,49 @@ class Canvas {
         this.pxSize = pxSize;
     }
 
-    canvasInit() {
-        console.log(`canvas width: ${this.cWidth} || canvas height: ${this.cHeight}`)
+    async canvasInit() {
+        console.log(`canvas width: ${this.cWidth} || canvas height: ${this.cHeight}`)        
         canvas.width = this.cWidth;
         canvas.height = this.cHeight;
-        
+
+
+    }
+
+    draw() {
+
+    }
+
+    update(t) {
+        let r, g, b = 0;
+        let tm = Math.sin(t)
+        console.log(tm)
 
 
         for (let y=0; y < this.cHeight/this.pxSize; y++) {
-            for (let x=0; x < this.cWidth/this.pxSize; x++) {
-                ctx.fillStyle = `rgb(${Math.sin(x/y)*255},${Math.sin(y/x)*255},${x-y})`;
+            r = tm*y*3;
+            g = -tm*y*3;
 
+
+
+
+            for (let x=0; x < this.cWidth/this.pxSize; x++) {
+
+                b = (Math.cos(t+Math.tan(x*t*y)))*255;
+
+
+                ctx.fillStyle = `rgb(${r},${g},${b})`;
                 ctx.fillRect(x*this.pxSize, y*this.pxSize, this.pxSize, this.pxSize);
             }
         }
     }
-
-    draw() {
-        console.log(this.pxRemainder)
-    }
 };
 
 
-const c = new Canvas(700, 700, 240);
-
+const c = new Canvas(100, 100, 1);
 c.canvasInit();
 
-canvas.addEventListener("click", c.draw());
+let t=0;
+window.setInterval(() => {t+=0.1; c.update(t)}, 100);
+
+let run = true;
+
